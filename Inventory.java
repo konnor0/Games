97fe1f5415cf4e0c032fc48c1c751b4aecdbe887
konnor0 <konnorscomputers@gmail.com>
2014-06-10 13:15:45 -0500

    Update Inventory.java

--- Inventory.java
+++ Inventory.java
@@ -1,13 +1,30 @@
 package konnorscomputers;
 
+
 /**
  * Created by konnor on 6/9/14.
  */
 public class Inventory {
     int itemSlotSelected;
     int maxItemSlots;
+    int itemIDSelected;
     Inventory(){
         itemSlotSelected = 1;
         maxItemSlots = 3;
+    }
+    int itemIDSelected(){
+        if(itemSlotSelected == 1){
+            itemIDSelected = 1 ;
+        }
+        else if(itemSlotSelected == 2){
+            itemIDSelected = 2 ;
+        }
+        else if(itemSlotSelected == 3){
+            itemIDSelected = 3;
+        }
+        else{
+            itemIDSelected = 0;
+        }
+        return itemIDSelected;
     }
 }
