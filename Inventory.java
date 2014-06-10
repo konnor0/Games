6d213763daa5f460a052123a6a55fb9e02a75d54
konnor0 <konnorscomputers@gmail.com>
2014-06-10 08:03:05 -0500

    Inventory.java
    do i need to explain this one?

---
+++ Inventory.java
@@ -0,0 +1,13 @@
+package konnorscomputers;
+
+/**
+ * Created by konnor on 6/9/14.
+ */
+public class Inventory {
+    int itemSlotSelected;
+    int maxItemSlots;
+    Inventory(){
+        itemSlotSelected = 1;
+        maxItemSlots = 3;
+    }
+}
